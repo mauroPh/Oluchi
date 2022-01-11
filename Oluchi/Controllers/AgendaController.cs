8052b51bdc03952e6b97468443f52bd0b9aa0dd8
mauroPh <mauro.philipe.santos@gmail.com>
2022-01-10 23:33:33 -0300

    Adicionado View de Busca na agenda

--- Oluchi/Controllers/AgendaController.cs
+++ Oluchi/Controllers/AgendaController.cs
@@ -3,150 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Oluchi.Models;
+using Oluchi.Services;
+
 
 namespace Oluchi.Controllers
 {
     public class AgendaController : Controller
     {
-        private readonly OluchiContext _context;
+        private readonly AgendaService _agendaService;
 
-        public AgendaController(OluchiContext context)
+        public AgendaController(AgendaService agendaService)
         {
-            _context = context;
+            _agendaService = agendaService;
         }
 
-        // GET: Agenda
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Agenda.ToListAsync());
-        }
-
-        // GET: Agenda/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var agenda = await _context.Agenda
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (agenda == null)
-            {
-                return NotFound();
-            }
-
-            return View(agenda);
-        }
-
-        // GET: Agenda/Create
-        public IActionResult Create()
+        public IActionResult Index()
         {
             return View();
         }
 
-        // POST: Agenda/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Date,Local,Apresentacoes,Status")] Agenda agenda)
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            if (ModelState.IsValid)
+            if (!minDate.HasValue)
             {
-                _context.Add(agenda);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
             }
-            return View(agenda);
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _agendaService.FindByDateAsync(minDate, maxDate);
+            return View(result);
         }
 
-        // GET: Agenda/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
-            if (id == null)
+            if (!minDate.HasValue)
             {
-                return NotFound();
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
             }
-
-            var agenda = await _context.Agenda.FindAsync(id);
-            if (agenda == null)
+            if (!maxDate.HasValue)
             {
-                return NotFound();
+                maxDate = DateTime.Now;
             }
-            return View(agenda);
-        }
-
-        // POST: Agenda/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Date,Local,Apresentacoes,Status")] Agenda agenda)
-        {
-            if (id != agenda.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(agenda);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AgendaExists(agenda.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(agenda);
-        }
-
-        // GET: Agenda/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var agenda = await _context.Agenda
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (agenda == null)
-            {
-                return NotFound();
-            }
-
-            return View(agenda);
-        }
-
-        // POST: Agenda/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var agenda = await _context.Agenda.FindAsync(id);
-            _context.Agenda.Remove(agenda);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool AgendaExists(int id)
-        {
-            return _context.Agenda.Any(e => e.Id == id);
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _agendaService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
         }
     }
 }
